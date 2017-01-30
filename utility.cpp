b7828e9e4493da1647a82edb3994cf162439a3c7
leian7 <leian@osuosl.org>
2017-01-29 19:36:39 -0800

    begin

---
+++ utility.cpp
@@ -0,0 +1,48 @@
+/* This file contains helper functions used by both the naive and enhanced
+ * versions of the closest-pair algorithms. */
+
+/* list of var names
+ * find_closest_pair
+ * points_arr
+ * n  <-- length of arr
+ * find_min_dist
+ * L
+ * find_L
+ * points_arr_left
+ * points_arr_right
+ * delta_1, delta_2, delta
+ * M_y
+ * merge_sort
+ * d_m
+ * closest_cross_pair
+ * presort_y_c
+ * presort_x_c
+ * curr_min, min
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+struct Coordinate {
+	float x;
+	float y;
+};
+
+/* Coordinate* merge() {
+}
+
+void merge_sort(Coordinate* points_arr, char c, int n, int start, int end) {
+	Coordinate *points_arr_left = new Coordinate[n/2];
+	Coordinate *points_arr_right = new Coordinate[n/2];
+
+} */	
+
+int main()
+{
+	return 0;
+}
+	
