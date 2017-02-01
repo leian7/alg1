05455cbc7ec29b1350ae7e03d629fbe12bf52d53
Yingshi Huang <huangyin@flip1.engr.oregonstate.edu>
2017-02-01 02:32:36 -0800

    add if statement to only print min dist pairs

--- as1.cpp
+++ as1.cpp
@@ -41,13 +41,11 @@ int main(){
 		f >> line[i].y;	
 		//cout << " In the second time "<<endl;
 	}
-	for(int o = 0; o < size; o++){
-		cout << " x is " << line[o].x << " y is " << line[o].y << endl;
-	}
+	
 	for(int i=size-1; i > 0; i--){
 		arraySize += i;
 	}
-	cout << arraySize << endl;
+	//cout << arraySize << endl;
 	Distance *Sorted = new Distance [arraySize];
 	int k = 0 ;
 	for(int i=0; i < size-1; i++){
@@ -120,31 +118,11 @@ int main(){
 
 
 	for(int i=0; i < arraySize-1; i++){
+		if(Sorted[i].dis == Sorted[0].dis)
 		cout << " ( " << line[Sorted[i].x].x << "," << line[Sorted[i].x].y << ")   " << "   ("<< line[Sorted[i].y].x <<"," <<  line[Sorted[i].y].y <<")   "<< " distances : " << Sorted[i].dis << endl;
 	}
 	return 0;
 	delete [] line;
 
 }
-
-
-
-/*
-int main(){
-
-	string line;
-	ifstream myfile("example.input");
-	if(myfile.is_open()){
-		while(getline(myfile,line)){
-			cout << line << '\n';
-		}
-		myfile.close();
-	
-	}
-	else cout << "Unable to open the file\n";
-
-	return 0;
-
-}
-*/
 
