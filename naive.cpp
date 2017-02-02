06f4ca5cea02f4df69f9a6c14a464ccebcfc5827
Anne Lei <annie.lei7@gmail.com>
2017-02-01 20:49:14 -0800

    organize files for naive

---
+++ naive.cpp
@@ -0,0 +1,39 @@
+#include "functions.cpp"
+int main() {
+	int size,count = 1;
+	float min;
+	
+	ifstream f;
+	f.open("example.input");
+	string pairs;
+	while (getline(f, pairs)){
+		size++;
+	}
+	f.close();
+	Coordinate *line = new Coordinate [size];
+	
+	f.open("example.input");
+
+	for(int i = 0; i < size; i++){
+		f >> line[i].x;
+		f >> line[i].y;	
+	}
+	
+	min = find_closest_pair(line, size);
+	cout << " Minimum : " << min << endl;	
+	//line = sort_by_x_and_y(size,line);
+	set< pair<Coordinate,Coordinate> >::iterator it;
+	for(it=st.begin(); it!=st.end(); ++it){
+		//first is x, y is the second element in one point.
+		if(sqrtf( pow(it->first.x - it->second.x , 2) + pow(it->first.y - it->second.y , 2))!=min  ){
+			st.erase(it);
+		}
+
+	}
+	for(int i=0; i < 1; i++){
+		cout<<"("<<st.begin()->first.x<<" "<<st.begin()->first.y<<")"<<"("<<st.begin()->second.x<<" "<<st.begin()->second.y<<")"<<endl;
+		count++;
+	}
+	delete[]line;
+	return 0;
+}
